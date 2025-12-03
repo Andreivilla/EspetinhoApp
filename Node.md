00ecfb90cb232d4611584c328573d710d3735d22
Andreivilla <andreivilla63@gmail.com>
2025-12-02 23:52:24 -0300

    chore: corrige o comando `start` para `start:next` chore: modifica o comando para comportar rodar o app em standalone `node .next/standalone/server.js` chore: adiciona os comandos `cpPublic:win`,`cpDb:win`,`cpStatic:win`, `cp:all:win`.

---
+++ Node.md
@@ -0,0 +1,3 @@
+chore: corrige o comando `start` para `start:next`
+chore: modifica o comando para comportar rodar o app em standalone `node .next/standalone/server.js`
+
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
+/*  images: {
     unoptimized: true,
   },
   experimental: {
@@ -8,6 +8,7 @@ const nextConfig = {
       bodySizeLimit: '50mb', 
     },
   },
+  */
   output: 'standalone',
 };
 
